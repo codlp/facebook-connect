cc2abbc8d771fb98c3e672eca961498c8a2b0f62
coralie delpha <coralie.delpha@gmail.com>
2020-07-01 22:54:07 +0200

    try to get user_friends

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@
 Devise.setup do |config|
   # Facebook connect
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-    scope: 'email',
+    scope: 'email, user_friends',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true
